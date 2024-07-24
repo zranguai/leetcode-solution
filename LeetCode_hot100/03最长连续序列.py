c6de87be56fe496a4e813fc13b782fcc17766650
zranguai <zranguai@gmail.com>
2024-07-24 22:33:34 +0800

    03最长子序列_start

---
+++ LeetCode_hot100/03最长连续序列.py
@@ -0,0 +1,18 @@
+# 2024-7-24 v0.1
+# https://leetcode.cn/problems/longest-consecutive-sequence/description/?envType=study-plan-v2&envId=top-100-liked
+# tag:哈希
+
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    nums = [100,4,200,1,3,2]
+    res_nums = solution.longestConsecutive(nums)
+    print(res_nums)
